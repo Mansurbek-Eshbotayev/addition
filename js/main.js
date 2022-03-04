cbcd38ac7642543a72b73ebeea391a7eb7d27d86
Mansurbek-Eshbotayev <mustafoimron1820@gmail.com>
2022-03-04 23:53:49 +0500

    inital commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let arrey = [1, 3, 5, 9,]
+
+
+function num(ar){
+let sum = 0;
+for(var i = 0; i < ar.length; i = i + 1){
+    sum = sum + ar[i];
+    }
+     return sum
+}
+console.log(num(arrey));
